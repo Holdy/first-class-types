4e37b1461da0fb17a64241692240b73509fd3709
Holdy <christopherholden@hotmail.com>
2021-09-22 20:10:43 +0100

    Notes;

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,6 +7,11 @@ test('bytes-per-second formatting', () => {
     expect(slow.humanified).toBe('5 b/s');
 });
 
+
+//devicesEndpoint = sut.URI.from('http://smartthings/');
+//token = sut.HttpBearerToken('g9wt9w4ti');
+//data = deviceEndpoint.resolveWith(token);
+
 test('bytes-per-second Kb formatting', () => {
     const slow = sut.BytesPerSecond.from(1025);
 
